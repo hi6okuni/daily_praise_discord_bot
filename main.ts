665cba7aa6c3e450612896f14f24cd715b99718e
hi6okuni <hi6okuni@gmail.com>
2024-06-22 09:20:18 +0900

    remove log

--- main.ts
+++ main.ts
@@ -2,7 +2,6 @@ import { atcoder } from "./atcoder.ts";
 import { leetcode } from "./leetcode.ts";
 
 Deno.cron("daily praise", "0 1 * * *", async () => {
-	console.log("fire?");
 	await leetcode();
 	await atcoder();
 });
